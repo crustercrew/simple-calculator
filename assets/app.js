d1bc9e0bb109bbc36ddc66f131bbf484a0367b55
Fahreal Bernov <fahrealbernov@gmail.com>
2023-04-04 12:56:17 +0700

    adding pecahan,akar,dan perpangkatan

--- assets/app.js
+++ assets/app.js
@@ -95,13 +95,50 @@ const inputDecimal = (dot) =>{
 }
 
 const percentage = document.querySelector('.percentage');
-percentage.addEventListener('click',(event)=>{
+percentage.addEventListener('click',()=>{
     inputPercentage(currentNumber);
     updateScreen(currentNumber);
 });
-const inputPercentage = (percent)=>{
-    if (percent === 0) {
+const inputPercentage = ()=>{
+    if (currentNumber === 0) {
         return;
     }
     currentNumber /= 100;
+}
+const remove = document.querySelector('.delete');
+remove.addEventListener('click',()=>{
+    removecurrent();
+    updateScreen(currentNumber);
+})
+
+const removecurrent = ()=>{
+    currentNumber = '0';
+}
+const pangkat = document.querySelector('.perpangkatan');
+pangkat.addEventListener('click',()=>{
+    hitungpangkat(currentNumber);
+    updateScreen(currentNumber);
+})
+const hitungpangkat = ()=>{
+    if (currentNumber === 0) {
+        return;
+    }
+    currentNumber **= 2;
+}
+const pecahanan = document.querySelector('.pecahan');
+pecahanan.addEventListener('click',()=>{
+    updateScreen(hitungpecahan(currentNumber));
+})
+const hitungpecahan = ()=>{
+    let pecahan = 1 / currentNumber;
+    return pecahan;
+}
+
+const akarpangkat = document.querySelector('.akar-pangkat');
+akarpangkat.addEventListener('click',()=>{
+    updateScreen(hitungakar(currentNumber));
+})
+const hitungakar = ()=>{
+    let akar = Math.sqrt(currentNumber);
+    return akar;
 }
